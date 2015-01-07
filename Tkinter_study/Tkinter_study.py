f85e40f5d7b1d00591c31da84f7a5ba4e0c8793c
Huang-Jacky <hjc853@live.com>
2015-01-07 21:20:19 +0800

    Update tkinter_study.py, remove encode button, add clear button

--- Tkinter_study/Tkinter_study.py
+++ Tkinter_study/Tkinter_study.py
@@ -3,9 +3,8 @@ __author__ = 'Administrator'
 from tkinter import *
 from tkinter import ttk
 import urllib
-
 from urllib.parse import *
-
+import re
 
 #
 # def timez():
@@ -16,17 +15,29 @@ from urllib.parse import *
 #         root.update()
 #         time.sleep(1)
 
-def abc():
+def urlToChar():
     aa = urllib.parse.unquote(ourl.get())
     text.delete(0.0, END)
     text.insert(INSERT, aa)
 
 
-def cba():
+def CharToUrl():
     aa = urllib.parse.quote(ourl.get())
     text.delete(0.0, END)
     text.insert(INSERT, aa)
 
+
+def judge():
+    rejudge=r'%\S\S%'
+    if re.findall(rejudge,ourl.get()) != []:
+        urlToChar()
+    else:
+        CharToUrl()
+        
+def clear():
+    text.delete(0.0, END)
+    ourl.set('')
+    oURL.focus_set()
 
 def selectAll(char):
     text.tag_add("sel", "1.0", "end")
@@ -38,8 +49,8 @@ root.resizable(False, False)
 ourl = StringVar()
 
 
-originalURL = Label(root, text='Before:', fg='red', bg='blue').grid(row=0, sticky=W)
-afterURL = Label(root, text='After:', fg='red', bg='blue').grid(row=1, sticky=W)
+originalURL = Label(root, text='Before:', fg='green', bg='white').grid(row=0, sticky=W)
+afterURL = Label(root, text='After:', fg='green', bg='white').grid(row=1, sticky=W)
 
 oURL = Entry(root, width=60, textvariable=ourl)
 oURL.grid(row=0, column=1, sticky=W)
@@ -55,7 +66,11 @@ style.map("C.TButton",
     background=[('pressed', '!disabled', 'black'), ('active', 'white')]
     )
 
-mButton = ttk.Button(root, text='Decode', command=abc, style='C.TButton').grid(row=3, column=3)
-nButton = ttk.Button(root, text='Encode', command=cba, style='C.TButton').grid(row=4, column=3)
+mButton = ttk.Button(root, text='Decode', command=judge, style='C.TButton').grid(row=3, column=3)
+nButton = ttk.Button(root, text='Clear', command=clear, style='C.TButton').grid(row=1, column=3)
 
 root.mainloop()
+
+####################################################################################################
+
+
